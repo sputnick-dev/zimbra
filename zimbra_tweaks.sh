1185bc29be39b5fecff3f6ad273c52b95b893a5b
sputnick <gilles.quenot@gmail.com>
2015-06-17 04:52:09 +0200

    added comments about ldap tweaks

--- zimbra_tweaks.sh
+++ zimbra_tweaks.sh
@@ -15,6 +15,9 @@ zmlocalconfig -e mailboxd_java_heap_new_size_percent=5
 zmlocalconfig -e tomcat_java_heap_memory_percent=40
 zmlocalconfig -e mailboxd_thread_stack_size=256k
 zmlocalconfig -e mailboxd_java_options="-server -Dhttps.protocols=TLSv1,TLSv1.1,TLSv1.2 -Djdk.tls.client.protocols=TLSv1,TLSv1.1,TLSv1.2 -Djava.awt.headless=true -Dsun.net.inetaddr.ttl=${networkaddress_cache_ttl} -Dorg.apache.jasper.compiler.disablejsr199=true -XX:+UseConcMarkSweepGC -XX:PermSize=64m -XX:MaxPermSize=128m -XX:SoftRefLRUPolicyMSPerMB=1 -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCApplicationStoppedTime -XX:-OmitStackTraceInFastThrow -Xloggc:/opt/zimbra/log/gc.log -XX:-UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:GCLogFileSize=4096K -Djava.net.preferIPv4Stack=true"
+# http://wiki.zimbra.com/wiki/OpenLDAP_Performance_Tuning_8.0
+# http://wiki.zimbra.com/wiki/LDAP_Multi_Master_Replication
+# http://wiki.zimbra.com/wiki/OpenLDAP_Tuning_Keys_8.0
 zmlocalconfig -e ldap_common_thread=4
 zmlocalconfig -e ldap_db_cachesize=2
 zmlocalconfig -e ldap_db_idlcachesize=6
